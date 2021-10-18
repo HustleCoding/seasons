1f355d745fd648835bd97eeb5b7155877fa6fbd1
HustleCoding <f.dobinciuc7@gmail.com>
2021-10-18 23:17:11 +0300

    [TASK 1] - learning about class components

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // we called setState!!!!
+                this.setState({ lat: position.coords.latitude });
+            },
+            err => console.log(err),
+        );
+    }
+
+    render() {
+        return <div>Latitude: {this.state.lat}</div>
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
